f8c4054bdb810de0d8c0bb2d40ca2964d95356ab
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-07-08 14:51:34 +0530

    Create 1991. Find the Middle Index in Array.py

---
+++ Easy/1991. Find the Middle Index in Array.py
@@ -0,0 +1,9 @@
+Link : https://leetcode.com/problems/find-the-middle-index-in-array/
+
+Code :
+
+def findMiddleIndex(self, nums: List[int]) -> int:
+    for i in range(len(nums)):
+        if sum(nums[:i]) == sum(nums[i+1:]):
+            return i
+    return -1
